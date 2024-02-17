d068d6f14b60faf0fc1f8be33f57b990ac093507
Luca <134801125+biso207@users.noreply.github.com>
2024-02-17 11:39:30 +0000

    tris 1.1

--- F_Classi/tris_cpp_bisognin.cpp
+++ F_Classi/tris_cpp_bisognin.cpp
@@ -9,7 +9,8 @@ using namespace std;
     grid == 'O' => cell filled by player2
 */
 class Tris {
-    public: // permette di utilizzare gli attributi anche al di fuori della classe
+    // zona 'public' => i metodi e attributi sono accessibili sia dentro che fuori della classe
+    public:
         char grid[3][3]; // matrice per la griglia => atributo della classe
 
         // metodo per lo stato iniziale della partita o avviarne una nuova
@@ -35,8 +36,9 @@ class Tris {
         // metodo per posizionare la 'X' del player1
         bool player_one(int x, int y) {
             // casi di errore
-            if (x > 2 || x < 0) return false;
-            if (y > 2 || y < 0) return false;
+            if (x > 3 || x < 1) return false;
+            if (y > 3 || y < 1) return false;
+            x-=1; y-=1;
             if (grid[x][y] == 'X' || grid[x][y] == 'O') return false;
 
             // caso di inserimento corretto
@@ -48,8 +50,9 @@ class Tris {
         // metodo per posizionare un 'O' del player2
         bool player_two(int x, int y) {
             // casi di errore
-            if (x > 2 || x < 0) return false;
-            if (y > 2 || y < 0) return false;
+            if (x > 3 || x < 1) return false;
+            if (y > 3 || y < 1) return false;
+            x-=1; y-=1;
             if (grid[x][y] == 'X' || grid[x][y] == 'O') return false;
 
             // caso di inserimento corretto
@@ -57,6 +60,75 @@ class Tris {
             return true;
         }
 
+        // metodo per controllare il vincitore della partita
+        int result, j=0;
+        int winner() {
+            for (int i=0; i<3; i++) {
+                result = control_column(i);
+                if (result != 0) return result;
+                result = control_row(i);
+                if (result != 0) return result;
+                for (int j=0; j<3; j++) {
+                    result = control_diagonal(i, j);
+                    if (result != 0) return result;
+                }
+            }
+        }
+
+    // zona 'private' => i metodi e attributi sono accessibili SOLO nella classe
+    private:
+        // metodo per controllare che una colonna sia riempita
+        int control_column(int col) {
+            // accumulatori per contare i valori nelle celle di una colonna
+            int acc_one = 0;
+            int acc_two = 0;
+            for (int i=0; i<3; i++) {
+                char cell = grid[i][col];
+                if (cell == 'X') acc_one++;
+                else if (cell == 'O') acc_two++;
+                else return 0;
+            }
+
+            if (acc_one == 3) return 1;
+            if (acc_two == 3) return 2;
+            return 0;
+
+        }
+
+        // metodo per controllare che una riga sia riempita
+        int control_row(int row) {
+            // accumulatori per contare i valori nelle celle di una riga
+            int acc_one = 0;
+            int acc_two = 0;
+            for (int i=0; i<3; i++) {
+                char cell = grid[row][i];
+                if (cell == 'X') acc_one++;
+                else if (cell == 'O') acc_two++;
+                else return 0;
+            }
+
+            if (acc_one == 3) return 1;
+            if (acc_two == 3) return 2;
+            return 0;
+
+        }
+
+        // metodo per controllare che una diagonale sia riempita
+        int control_diagonal(int row, int col) {
+            // accumulatori per contare i valori nelle celle di una diagonale
+            int acc_one = 0;
+            int acc_two = 0;
+            for (int i=0; i<3; i++) {
+                char cell = grid[row][col];
+                if (cell == 'X') acc_one++;
+                else if (cell == 'O') acc_two++;
+                else return 0;
+            }
+
+            if (acc_one == 3) return 1;
+            if (acc_two == 3) return 2;
+            return 0;
+        }
 };
 
 int main() {
@@ -69,42 +141,59 @@ int main() {
     cout << "TRIS" << endl;
     my_tris.print_grid(); // stampa dello sta iniziale della griglia
 
-    int x, y;
+    int x, y, cont=0, winner;
     bool valid_hit;
 
-    // mossa del giocatore 1
-    do {
-        cout << endl << "player 1" << endl;
-        cout << "digit valid coordinate" << endl;
-        cout << "X: ";
-        cin >> x;
-        cout << "Y: ";
-        cin >> y;
-        valid_hit = my_tris.player_one(x, y); // richiamo metodo per posizionare la 'X' del player 1
-        if (!valid_hit) cout << "invalid coordinate or already selected" << endl;
-        else cout << "valid coordinate" << endl;
-    } while (!valid_hit); // negato perché nel caso di false l'utente può reinserire altri valori
-    
-    // stampa dello stato della griglia
-    cout << endl;
-    my_tris.print_grid();
-    
-    // mossa del giocatore 2
-    do {
-        cout << endl << "player 2" << endl;
-        cout << "digit valid coordinate" << endl;
-        cout << "X: ";
-        cin >> x;
-        cout << "Y: ";
-        cin >> y;
-        valid_hit = my_tris.player_two(x, y); // richiamo metodo per posizionare la 'X' del player 1
-        if (!valid_hit) cout << "invalid coordinate or already selected" << endl;
-        else cout << "valid coordinate" << endl;
-    } while (!valid_hit); // negato perché nel caso di false diventa true e l'utente può reinserire altri valori
+    // ciclo principale del gioco
+    while (cont < 9) {
+        // mossa del giocatore 1
+        do {
+            cout << endl << "player 1" << endl;
+            cout << "digit valid coordinate" << endl;
+            cout << "X: ";
+            cin >> x;
+            cout << "Y: ";
+            cin >> y;
+            valid_hit = my_tris.player_one(x, y); // richiamo metodo per posizionare la 'X' del player 1
+            if (!valid_hit) cout << "invalid coordinate or already selected" << endl;
+            else cout << "valid coordinate" << endl;
+        } while (!valid_hit); // negato perché nel caso di false l'utente può reinserire altri valori
+        
+        // stampa dello stato della griglia
+        cout << endl;
+        my_tris.print_grid();
 
-    // stampa dello stato della griglia
-    cout << endl;
-    my_tris.print_grid();
+        // controllo dello stato della partita
+        winner = my_tris.winner();
+        if (winner != 0) break;
+        
+        // mossa del giocatore 2
+        do {
+            cout << endl << "player 2" << endl;
+            cout << "digit valid coordinate" << endl;
+            cout << "X: ";
+            cin >> x;
+            cout << "Y: ";
+            cin >> y;
+            valid_hit = my_tris.player_two(x, y); // richiamo metodo per posizionare la 'X' del player 1
+            if (!valid_hit) cout << "invalid coordinate or already selected" << endl;
+            else cout << "valid coordinate" << endl;
+        } while (!valid_hit); // negato perché nel caso di false diventa true e l'utente può reinserire altri valori
+
+        // stampa dello stato della griglia
+        cout << endl;
+        my_tris.print_grid();
+
+        // controllo dello stato della partita
+        winner = my_tris.winner();
+        if (winner != 0) break;
+        
+        cont+=2;
+    }
+
+    if (winner == 1) cout << endl << "player 1 win!" << endl;
+    else if (winner == 2) cout << endl << "player 2 win!" << endl;
+    else cout << "draw!" << endl;
 
     return 0;
 }
