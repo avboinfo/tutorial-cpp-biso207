1d81f2dff7fa28e547d1fc0d2f0135705c42f5d6
Luca <134801125+biso207@users.noreply.github.com>
2024-02-22 21:40:30 +0000

    tris 1.3

--- F_Classi/tris_cpp_bisognin.cpp
+++ F_Classi/tris_cpp_bisognin.cpp
@@ -17,18 +17,21 @@ class Tris {
         void reset_tabella() {
             for (int i=0; i<3; i++) {
                 for (int j=0; j<3; j++) {
-                    grid[i][j] = '+'; // riempimento della griglia di zeri
+                    grid[i][j] = ' '; // riempimento della griglia di zeri
                 }
             }
         }
 
         // metodo per stampare il contenuto della griglia ad ogni click degli utenti
         void print_grid() {
+            cout << "  |  1  |  2  |  3  |" << endl;
+            cout << "---------------------" << endl;
             for (int i=0; i<3; i++) {
+                cout << i+1 << " |  ";
                 for (int j=0; j<3; j++) {
-                    cout << grid[i][j] << "\t";
+                    cout << grid[i][j] << "  |  ";
                 }
-                cout << endl;
+                cout << endl << "---------------------" << endl;
             }
         }
 
@@ -68,11 +71,10 @@ class Tris {
                 if (result != 0) return result;
                 result = control_row(i);
                 if (result != 0) return result;
-                for (int j=0; j<3; j++) {
-                    result = control_diagonal(i, j);
-                    if (result != 0) return result;
-                }
+                result = control_diagonal();
+                if (result != 0) return result;
             }
+            return 0;
         }
 
     // zona 'private' => i metodi e attributi sono accessibili SOLO nella classe
@@ -114,19 +116,18 @@ class Tris {
         }
 
         // metodo per controllare che una diagonale sia riempita
-        int control_diagonal(int row, int col) {
+        int control_diagonal() {
             // accumulatori per contare i valori nelle celle di una diagonale
             int acc_one = 0;
             int acc_two = 0;
-            for (int i=0; i<3; i++) {
-                char cell = grid[row][col];
-                if (cell == 'X') acc_one++;
-                else if (cell == 'O') acc_two++;
-                else return 0;
-            }
-
-            if (acc_one == 3) return 1;
-            if (acc_two == 3) return 2;
+            if (grid[0][0] == 'X' && grid[1][1] == 'X' && grid[2][2] == 'X')
+                return 1;
+            if (grid[0][2] == 'X' && grid[1][1] == 'X' && grid[2][0] == 'X')
+                return 1;
+            if (grid[0][0] == 'O' && grid[1][1] == 'O' && grid[2][2] == 'O')
+                return 2;
+            if (grid[0][2] == 'O' && grid[1][1] == 'O' && grid[2][0] == 'O')
+                return 2;
             return 0;
         }
 };
@@ -138,7 +139,7 @@ int main() {
     // init grid
     my_tris.reset_tabella();
 
-    cout << "TRIS" << endl;
+    cout << "TRIS" << endl; 
     my_tris.print_grid(); // stampa dello sta iniziale della griglia
 
     int x, y, cont=0, winner;
@@ -149,16 +150,19 @@ int main() {
         // mossa del giocatore 1
         do {
             cout << endl << "player 1" << endl;
-            cout << "digit valid coordinate" << endl;
+            cout << "digit your position" << endl;
             cout << "X: ";
             cin >> x;
             cout << "Y: ";
             cin >> y;
             valid_hit = my_tris.player_one(x, y); // richiamo metodo per posizionare la 'X' del player 1
             if (!valid_hit) cout << "invalid coordinate or already selected" << endl;
-            else cout << "valid coordinate" << endl;
+            else {
+                cont++;
+                cout << "valid coordinate" << endl;
+            }
         } while (!valid_hit); // negato perché nel caso di false l'utente può reinserire altri valori
-        
+
         // stampa dello stato della griglia
         cout << endl;
         my_tris.print_grid();
@@ -166,18 +170,22 @@ int main() {
         // controllo dello stato della partita
         winner = my_tris.winner();
         if (winner != 0) break;
+        else if (cont == 9) break;
         
         // mossa del giocatore 2
         do {
             cout << endl << "player 2" << endl;
-            cout << "digit valid coordinate" << endl;
+            cout << "digit your position" << endl;
             cout << "X: ";
             cin >> x;
             cout << "Y: ";
             cin >> y;
             valid_hit = my_tris.player_two(x, y); // richiamo metodo per posizionare la 'X' del player 1
             if (!valid_hit) cout << "invalid coordinate or already selected" << endl;
-            else cout << "valid coordinate" << endl;
+            else {
+                cont++;
+                cout << "valid coordinate" << endl;
+            }
         } while (!valid_hit); // negato perché nel caso di false diventa true e l'utente può reinserire altri valori
 
         // stampa dello stato della griglia
@@ -187,13 +195,12 @@ int main() {
         // controllo dello stato della partita
         winner = my_tris.winner();
         if (winner != 0) break;
-        
-        cont+=2;
+
     }
 
     if (winner == 1) cout << endl << "player 1 win!" << endl;
     else if (winner == 2) cout << endl << "player 2 win!" << endl;
-    else cout << "draw!" << endl;
+    else cout << endl << "draw!" << endl;
 
     return 0;
 }
