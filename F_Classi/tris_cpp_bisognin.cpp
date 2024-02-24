39ea933a48c58d7adcf44148b77132af2c28af23
Luca <134801125+biso207@users.noreply.github.com>
2024-02-24 11:31:11 +0000

    tris 1.5

--- F_Classi/tris_cpp_bisognin.cpp
+++ F_Classi/tris_cpp_bisognin.cpp
@@ -24,14 +24,15 @@ class Tris {
 
         // metodo per stampare il contenuto della griglia ad ogni click degli utenti
         void print_grid() {
-            cout << "  |  1  |  2  |  3  |" << endl;
-            cout << "---------------------" << endl;
+            cout << "_______________________" << endl;
+            cout << "    |  1  |  2  |  3  |" << endl;
+            cout << "-----------------------" << endl;
             for (int i=0; i<3; i++) {
-                cout << i+1 << " |  ";
+                cout << "| " << i+1 << " |  ";
                 for (int j=0; j<3; j++) {
                     cout << grid[i][j] << "  |  ";
                 }
-                cout << endl << "---------------------" << endl;
+                cout << endl << "-----------------------" << endl;
             }
         }
 
@@ -155,7 +156,7 @@ int main() {
     // init grid
     my_tris.reset_tabella();
 
-    cout << "TRIS" << endl; 
+    cout << "TRIS" << endl << endl; 
     my_tris.print_grid(); // stampa dello sta iniziale della griglia
 
     int x, y, cont=0, winner;
