e16da33b9e45ef8447418c4bfbdcf9b5d70a547c
Luca <134801125+biso207@users.noreply.github.com>
2024-02-26 08:43:40 +0000

    lunedì 26/02/2024

--- F_Classi/Bicicletta.cpp
+++ F_Classi/Bicicletta.cpp
@@ -10,23 +10,41 @@ class Bicicletta {
   public:
     int raggio;
     string colore;
-    // metodo costruttore => chiamato in automatico ogni volta che si crea un parametro della classe
+    // metodo costruttore privo di parametri => chiamato in automatico ogni volta che si crea un parametro della classe
     Bicicletta() {
         raggio=10;
         colore="bianco";
     }
 
-    // metodo pubblico della classe
-    void presentati() {
-        cout << "Buongiorno, sono una bici di colore " << colore << " e con le ruote " << raggio << "!" << endl;
+    // metodo costruttore con parametri
+    Bicicletta (int r, string c) {
+        raggio = r;
+        colore = c;
+    }
+
+    // metodi pubblici della classe
+    void bici_base() {
+        cout << endl << "la bici di fabbrica è di colore " << colore << " e con le ruote " << raggio << "." << endl;
+    }
+    void bici_pers() {
+        cout << endl << "la bici da te creata è di colore " << colore << " e con le ruoute " << raggio << "." << endl;
     }
 };
 
 int main()
 {
     Bicicletta x;
+    x.bici_base();
 
-    x.presentati();
+    cout << endl << "digita le caratteristiche che vuoi che abbia la tua bici." << endl;
+    int r; string c;
+    cout << endl << "raggio: ";
+    cin >> r;
+    cout << "colore: ";
+    cin >> c;
+
+    Bicicletta y(r, c);
+    y.bici_pers();
 
     return 0;
 }
