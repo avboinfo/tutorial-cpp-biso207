377aa23a88e6775524041b3f36b4053dc5ed10f8
Luca <134801125+biso207@users.noreply.github.com>
2024-03-23 09:41:59 +0000

    mastermind 1.4

--- X_MasterMind/main_mastermind_cpp_Bisognin.cpp
+++ X_MasterMind/main_mastermind_cpp_Bisognin.cpp
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[]) {
     int cont = 0, attempts = 0;
     bool stop = true;
 
-    cout << "MASTERMIND" << endl;
+    cout << endl << "MASTERMIND" << endl;
 
     mastermind gioco = mastermind();
     while (stop) {
